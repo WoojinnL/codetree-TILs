d51776b4684636a86db49fbe5a1043d088799e45
WoojinnL <110981128+WoojinnL@users.noreply.github.com>
2024-07-17 19:23:33 +0900

    Title: 함수를 이용한 369 게임, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/함수를 이용한 369 게임/369-games-using-functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int IsInThree(int i) {
+    int sum = 0;
+    while (i != 0) {
+        if ((i % 10) % 3 == 0)
+            sum++;
+        i = i / 10;
+    }
+    if (sum != 0)
+        return 1;
+    else
+        return 0;
+}
+
+int IsThreeTimes(int i) {
+    if (i % 3 == 0 || IsInThree(i) == 1)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int a, b, num = 0;
+    scanf("%d %d", &a, &b);
+    for (int i = a; i <= b; i++) {
+        if (IsThreeTimes(i) == 1)
+            num++;
+    }
+    printf("%d", num);
+    return 0;
+}
