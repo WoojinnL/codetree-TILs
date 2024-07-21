b6f9c43b07c0a19b7aa307ea6f1a971f263dd944
WoojinnL <110981128+WoojinnL@users.noreply.github.com>
2024-07-21 20:13:53 +0900

    Title: 단어 정렬, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240721/단어 정렬/sorting-words.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void swap(char* a, char* b){
+    char temp[100];
+    strcpy(temp, a);
+    strcpy(a, b);
+    strcpy(b, temp);
+}
+
+void sort(char str[][100], int n){
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(strcmp(str[i], str[j]) > 0)
+                swap(str[i], str[j]);
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    char str[100][100];
+    for(int i = 0; i < n; i++)
+        scanf("%s", str[i]);
+    
+    sort(str, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%s\n", str[i]);
+    return 0;
+}
