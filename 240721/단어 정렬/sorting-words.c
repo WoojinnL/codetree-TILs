d2d20b4bef6fd7cc93fa75c07267ee48b0a8a407
WoojinnL <110981128+WoojinnL@users.noreply.github.com>
2024-07-21 20:17:57 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/단어 정렬/sorting-words.c
+++ 240721/단어 정렬/sorting-words.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 void swap(char* a, char* b){
-    char temp[100];
+    char temp[101];
     strcpy(temp, a);
     strcpy(a, b);
     strcpy(b, temp);
 }
 
-void sort(char str[][100], int n){
+void sort(char str[][101], int n){
     for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
             if(strcmp(str[i], str[j]) > 0)
@@ -21,7 +21,7 @@ void sort(char str[][100], int n){
 int main() {
     int n;
     scanf("%d", &n);
-    char str[100][100];
+    char str[101][101];
     for(int i = 0; i < n; i++)
         scanf("%s", str[i]);
     
