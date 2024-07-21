b87746295c0697253987829f604618a3b783af70
WoojinnL <110981128+WoojinnL@users.noreply.github.com>
2024-07-21 21:02:16 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort(char str[], int len){
+    char temp;
+    for(int i = 0; i < len; i++){
+        for(int j = i + 1; j < len; j++){
+            if(str[i] > str[j]){
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+
+int Check(char str1[], char str2[], int len){
+    for(int i = 0; i < len; i++){
+        if(str1[i] != str2[i])
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    char str1[100000];
+    char str2[100000];
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    int len = strlen(str1);
+
+    sort(str1, len);
+    sort(str2, len);
+
+    if(Check(str1, str2, len) == 1)
+        printf("Yes");
+    else
+        printf("No");
+    return 0;
+}
