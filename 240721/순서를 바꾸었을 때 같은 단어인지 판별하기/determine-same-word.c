86b240cf4a8ad4c7c237d04f203e834da9af7a1b
WoojinnL <110981128+WoojinnL@users.noreply.github.com>
2024-07-21 21:04:49 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 1086ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240721/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
+++ 240721/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-void sort(char str[], int len){
+void sort(char str[], int len) {
     char temp;
-    for(int i = 0; i < len; i++){
-        for(int j = i + 1; j < len; j++){
-            if(str[i] > str[j]){
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
                 temp = str[i];
                 str[i] = str[j];
                 str[j] = temp;
@@ -14,9 +14,9 @@ void sort(char str[], int len){
     }
 }
 
-int Check(char str1[], char str2[], int len){
-    for(int i = 0; i < len; i++){
-        if(str1[i] != str2[i])
+int Check(char str1[], char str2[], int len) {
+    for (int i = 0; i < len; i++) {
+        if (str1[i] != str2[i])
             return 0;
     }
     return 1;
@@ -28,12 +28,16 @@ int main() {
     scanf("%s", str1);
     scanf("%s", str2);
 
-    int len = strlen(str1);
+    int len;
+    if (strlen(str1) > strlen(str2))
+        len = strlen(str1);
+    else
+        len = strlen(str2);
 
     sort(str1, len);
     sort(str2, len);
 
-    if(Check(str1, str2, len) == 1)
+    if (Check(str1, str2, len) == 1)
         printf("Yes");
     else
         printf("No");
